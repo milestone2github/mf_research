d267683f0a3c8100154275983ebea4b07c16987a
Kishan Kumar <singhkishan9911@gmail.com>
2024-05-14 16:45:18 +0530

    modified import statement in index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './Redux/Store.js';
+import store from './redux/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
