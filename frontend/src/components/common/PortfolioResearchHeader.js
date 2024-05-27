32cb5ca0fe24b58262286db9d66f48a1f6d6a5fc
Kishan Kumar <singhkishan9911@gmail.com>
2024-05-27 16:30:54 +0530

    Add back button to Portfolio analysis

--- frontend/src/components/common/PortfolioResearchHeader.js
+++ frontend/src/components/common/PortfolioResearchHeader.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BackButton from "./BackButton";
 
 const Header = ({ title }) => {
   return (
-    <div className="header">
-      <Link to="/portfolio-analysis" className="back-button">
-        Back to Portfolio Analysis
-      </Link>
+    <div className="header relative">
+      <span className="absolute left-0 top-1/2 -translate-y-1/2">
+        <BackButton action={() => {window.history.back()}}/>
+      </span>
       <h1 className="page-title">{title}</h1>
     </div>
   );
