6708a5c94a0da5ee9ade4e3d88d1cbdb99461910
Kishan Kumar <singhkishan9911@gmail.com>
2024-06-22 10:49:02 +0530

    add autofoucus and disable pasting in input box in payout confirm modal

--- frontend/src/components/common/PayoutConfirmModal.jsx
+++ frontend/src/components/common/PayoutConfirmModal.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
-function PayoutConfirmModal({isOpen, title, handleCancel, handleProceed, error}) {
+function PayoutConfirmModal({ isOpen, title, handleCancel, handleProceed, error }) {
   const [inputValue, setInputValue] = useState('')
 
   useEffect(() => {
-    if(!isOpen) {
+    if (!isOpen) {
       setInputValue('')
     }
   }, [isOpen])
@@ -14,23 +14,25 @@ function PayoutConfirmModal({isOpen, title, handleCancel, handleProceed, error})
     handleProceed(inputValue)
   }
 
-  if(!isOpen) return null
+  if (!isOpen) return null
 
   return (
     <div className='absolute inset-0 z-[1001] w-screen h-screen bg-black/70 flex items-center justify-center'>
       <form onSubmit={handleSubmit} className={`w-fit h-fit bg-primary-white rounded-md shadow-md p-6 py-8 md:w-[428px] flex flex-col gap-y-4`}>
         <p className="text-gray-800 font-medium">{title}</p>
         <div className="flex flex-col">
-        <input 
-          type="text" 
-          name="ass-name" 
-          id="ass-name"
-          autoComplete='off' 
-          className='rounded-md p-2 w-full border border-gray-500' 
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        {error && <p className='text-red-400 text-sm text-left mt-1'>{error}</p>}
+          <input
+            onPaste={(e)=>{e.preventDefault()}}
+            autoFocus
+            type="text"
+            name="ass-name"
+            id="ass-name"
+            autoComplete='off'
+            className='rounded-md p-2 w-full border border-gray-500'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          {error && <p className='text-red-400 text-sm text-left mt-1'>{error}</p>}
         </div>
         <div className='flex gap-x-3 mt-4 justify-end'>
           <button onClick={handleCancel} type='button' className='border rounded-lg py-2 px-6 text-gray-800 hover:bg-gray-200'>Cancel</button>
