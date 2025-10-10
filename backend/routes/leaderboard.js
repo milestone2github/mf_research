bd1f679c3c2ef4181a3bf1224fb49b19804e3a58
Kishan Kumar <singhkishan9911@gmail.com>
2025-10-10 17:14:44 +0530

    add: insurance performance [leaderboardAudit] routes and controller

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { getLeaderboard } = require('../controllers/leaderboardController');
-const { lumpsumAudit } = require('../controllers/leaderboardPerformance');
+const { lumpsumAudit, leaderboardAudit } = require('../controllers/leaderboardPerformance');
 const verifyUser = require('../middlewares/VerifyUser');
 
 // Get leaderboard data
@@ -10,5 +10,8 @@ router.get('/', getLeaderboard);
 
 // Leaderboard performance and individual status view routes
 router.get("/performance/lumpsum-audit", verifyUser, lumpsumAudit);
+
+// GET individual performance data of insurance leaderboard
+router.get("/performance/leaderboard-audit", verifyUser, leaderboardAudit);
 
 module.exports = router;
