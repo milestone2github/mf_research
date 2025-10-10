e8da42c9e0f3f46bcd274e14e761d6398d21ff9e
Sunny Chaudhary <87089998+rigs05@users.noreply.github.com>
2025-10-10 17:15:29 +0530

    update: added MF_SIP_Data API

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { getLeaderboard } = require('../controllers/leaderboardController');
-const { lumpsumAudit } = require('../controllers/leaderboardPerformance');
+const { lumpsumAudit, mfSIP } = require('../controllers/leaderboardPerformance');
 const verifyUser = require('../middlewares/VerifyUser');
 
 // Get leaderboard data
@@ -10,5 +10,10 @@ router.get('/', getLeaderboard);
 
 // Leaderboard performance and individual status view routes
 router.get("/performance/lumpsum-audit", verifyUser, lumpsumAudit);
+
+// Get MF-SIP data
+router.get("/performance/sip-audit", verifyUser, mfSIP);
+
+
 
 module.exports = router;
